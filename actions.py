2bcc78e9c9fb7626e8fc69bd9a999a6c167ab362
Rohan <rohanduaSP@bitbucket.org>
2021-03-28 16:18:09 +0530

    Change form-policy#1 :: implement forms for restaurant - search

--- actions.py
+++ actions.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
-
-from rasa_sdk import Action
 from rasa_sdk.events import SlotSet
+from rasa_sdk.forms import FormAction
+from typing import Any, Text, Dict, List, Union
+from rasa_sdk import Action,Tracker
+
 import pandas as pd
 import json
 import smtplib
@@ -108,3 +110,47 @@ class ActionCheckLocation(Action):
 		else :
 			return [SlotSet('check_loc_flag',True)]
 		
+class RestaurantForm(FormAction):
+	def name(self):
+        return "restaurant_form"
+
+    def required_slots(tracker):
+
+        if tracker.get_slot('confirm_search') == True:
+            return ["confirm_search", "exercise", "sleep",
+             "diet", "stress", "goal"]
+        else:
+            return ["confirm_exercise", "sleep",
+             "diet", "stress", "goal"]
+		
+	def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+	
+		return {
+			"confirm_exercise": [
+				self.from_intent(intent="affirm", value=True),
+				self.from_intent(intent="deny", value=False),
+				self.from_intent(intent="inform", value=True),
+			],
+			"sleep": [
+				self.from_entity(entity="sleep"),
+				self.from_intent(intent="deny", value="None"),
+			],
+			"diet": [
+				self.from_text(intent="inform"),
+				self.from_text(intent="affirm"),
+				self.from_text(intent="deny"),
+			],
+			"goal": [
+				self.from_text(intent="inform"),
+			],
+		}
+	
+	def submit(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+
+        dispatcher.utter_message("Thanks, great job!")
+        return []
